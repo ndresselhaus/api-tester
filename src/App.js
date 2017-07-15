9b42d836023848042ff4dd293c6e8bbb00326116
Bronwyn Perry-Huston <bronhuston@gmail.com>
2017-07-15 15:00:09 -0700

    Add Generate Things functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {parse as qsParse} from 'qs';
 
 import UrlInput from './UrlInput';
 import RequestBodyInput from './RequestBodyInput';
@@ -10,13 +11,21 @@ class App extends Component {
     super();
     this.state = {
       url: '',
-      requestBodyInput: ''
+      requestBodyInput: '',
     };
   }
 
   handleChange = (key, e) => {
-    console.log(key, e.target.value);
     this.setState({[key]: e.target.value});
+  };
+
+  generateThings = () => {
+    const {url, requestBodyInput: requestBody} = this.state;
+    let queryParams = qsParse(url.substring(url.indexOf('?') + 1));
+    let bodyParams = JSON.parse(requestBody);
+
+    this.setState({queryParams, bodyParams});
+
   };
 
   render() {
@@ -30,7 +39,21 @@ class App extends Component {
           <UrlInput url={this.state.url} onChange={(e) => this.handleChange('url', e)}/>
         </p>
         <div>
-          <RequestBodyInput requestBody={this.state.requestBody} onChange={(e) => this.handleChange('requestBodyInput', e)}/>
+          <RequestBodyInput
+            requestBody={this.state.requestBody}
+            onChange={(e) => this.handleChange('requestBodyInput', e)}/>
+        </div>
+        <div>
+          <button onClick={this.generateThings}>Generate Things</button>
+        </div>
+        <div style={{padding: 5, border: 1}}>
+          {this.state.queryParams && Object.keys(this.state.queryParams).map(parameter => {
+            return <label key={parameter}>{parameter}<input type="text"/></label>
+          })}
+          {this.state.bodyParams && Object.keys(this.state.bodyParams).map(bodyParam => {
+              return <label key={bodyParam}>{bodyParam}<input type="text"/></label>
+            }
+          )}
         </div>
       </div>
     );
